fe8be499bc08dc361f2d7effd80f2c5c200e38ca
charles7668 <charles76680119@gmail.com>
2024-09-13 11:03:56 +0800

    UserSettingProvider do not show text , just return undefined

--- aiba-ui/src/components/UserSettingProvider.tsx
+++ aiba-ui/src/components/UserSettingProvider.tsx
@@ -7,22 +7,18 @@ export const UserSettingProvider: React.FC<{ children: ReactNode }> = ({
   children,
 }) => {
   const [userSetting, setUserSetting] = useState<UserSetting | undefined>();
-  const [isFailed, setIsFailed] = useState<boolean>(false);
   useEffect(() => {
     Api.getUserSetting().then((response) => {
       if (response.status !== 200) {
-        setIsFailed(true);
+        setUserSetting(undefined);
         return;
       }
       response.json().then((data) => {
         setUserSetting(data);
       });
-      setIsFailed(false);
     });
   }, []);
 
-  if (isFailed)
-    return <div>Failed to load user settings. Please try again later.</div>;
   return (
     <UserSettingContext.Provider value={{ userSetting, setUserSetting }}>
       {children}
